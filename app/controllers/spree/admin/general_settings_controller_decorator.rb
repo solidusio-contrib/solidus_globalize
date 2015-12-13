5866f82c6fdef2a95ce1d5b8c4142b15d1e0875d
Serp <serg.tps@gmail.com>
2015-12-13 15:56:46 +0200

    ref for working with solidus_i18n

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -1,11 +1,12 @@
 Spree::Admin::GeneralSettingsController.class_eval do
-  before_filter :update_i18n_settings, only: :update
+  before_action :update_globalize_settings, only: :update
 
   private
-    def update_i18n_settings
-      params.each do |name, value|
-        next unless SolidusGlobalize::Config.has_preference? name
-        SolidusGlobalize::Config[name] = value.map(&:to_sym)
-      end
+
+  def update_globalize_settings
+    params.each do |name, value|
+      next unless SolidusGlobalize::Config.has_preference? name
+      SolidusGlobalize::Config[name] = value.map(&:to_sym)
     end
+  end
 end
