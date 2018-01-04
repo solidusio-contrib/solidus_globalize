b0c13f0d570503f64af6d9bf9bf30b84e7196f02
Alessandro Baccanelli <alessandro.baccanelli@archimedianet.it>
2018-01-04 13:03:30 +0100

    it's better to make a concern to override the method
    the solidus validator class make an override of the initialization and initialize the @field.

---
+++ app/models/concerns/solidus_globalize/db_maximum_length_validator.rb
@@ -0,0 +1,19 @@
+module SolidusGlobalize
+  module DbMaximumLengthValidator
+    extend ActiveSupport::Concern
+
+    included do
+
+      def validate(record)
+        field = @field.to_sym
+        limit = record.column_for_attribute(field).limit
+        value = record[field]
+        if value && limit && value.to_s.length > limit
+          record.errors.add(field, :too_long, count: limit)
+        end
+      end
+
+    end
+
+  end
+end
