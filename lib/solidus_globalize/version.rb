0dce49c131368a6ebbfe1e8b9145c5ee8fbd1bde
Noel <noeldiaz83@gmail.com>
2019-10-08 15:56:34 +0100

    bug in db_maximum_lenght_validator

--- lib/solidus_globalize/version.rb
+++ lib/solidus_globalize/version.rb
@@ -10,7 +10,7 @@ module SolidusGlobalize
   module VERSION
     MAJOR = 3
     MINOR = 1
-    TINY  = 1
+    TINY  = 2
     PRE   = "beta"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
