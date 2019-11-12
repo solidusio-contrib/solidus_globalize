019b453d84089f17d219053b2dd7a0117bd9f4c9
Alberto Vena <kennyadsl@gmail.com>
2019-11-12 16:46:13 +0100

    Use the right syntax for DbMaximumLengthValidator
    Not sure how could it be working before this fix...

--- app/models/spree/validations/db_maximum_length_validator_decorator.rb
+++ app/models/spree/validations/db_maximum_length_validator_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   module Validations
     class DbMaximumLengthValidator < ActiveModel::Validator
       def validate(record)
-        field = @field.to_sym
+        field = options[:field]
         limit = record.column_for_attribute(field).limit
         value = record[field]
         if value && limit && value.to_s.length > limit
