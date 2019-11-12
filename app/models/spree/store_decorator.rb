351409ef237f4875d14f8a2f9a6675966b788f22
Alberto Vena <kennyadsl@gmail.com>
2019-11-12 16:39:16 +0100

    Change StoreDecorator to use prepend instead of class_eval
    This fixes a load issue since there is a definition of Spree::Store
    as a module loaded before this decorator.

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,7 +1,13 @@
 module Spree
-  Store.class_eval do
-    translates :name, :meta_description, :meta_keywords, :seo_title,
-               fallbacks_for_empty_translations: true
-    include SolidusGlobalize::Translatable
+  module StoreDecorator
+    def self.prepended(base)
+      base.class_eval do
+        translates :name, :meta_description, :meta_keywords, :seo_title,
+                   fallbacks_for_empty_translations: true
+        include SolidusGlobalize::Translatable
+      end
+    end
+
+    ::Spree::Store.prepend self
   end
 end
