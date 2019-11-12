64da576cc3bf006cab64a9849f311f0d9a8f182f
Alberto Vena <kennyadsl@gmail.com>
2019-11-12 16:41:28 +0100

    Override Spree::Store.available_locales
    Core method uses super but we need to avoid that since
    the same method is defined into globalize:
    
    https://github.com/globalize/globalize/blob/8f1e84942a1bbe4e77ae2ce87450c43eb447bbaa/lib/globalize/active_record/instance_methods.rb#L153-L155
    
    We should probably backport this change into core, since super()
    here is used just to get the database value.

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -8,6 +8,19 @@ module Spree
       end
     end
 
+    def available_locales
+      # TODO, we should probably backport this change into core.
+      # Instead of calling super we read the database attribute
+      # Globalize overrides available_locales and super is returning
+      # something an array, while we expect a string.
+      locales = read_attribute(:available_locales)
+      if locales
+        locales.split(",").map(&:to_sym)
+      else
+        Spree.i18n_available_locales
+      end
+    end
+
     ::Spree::Store.prepend self
   end
 end
