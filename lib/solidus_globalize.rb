0164b6d119f24685815b000d4f104788d3f2aabb
Alberto Vena <kennyadsl@gmail.com>
2019-12-03 17:11:21 +0100

    Include SolidusSupport::EngineExtensions::Decorators
    This allows to autoload and include all decorator classes
    correctly.

--- lib/solidus_globalize.rb
+++ lib/solidus_globalize.rb
@@ -5,5 +5,7 @@ require 'coffee_script'
 require 'solidus_support'
 require 'deface'
 
+require 'solidus_globalize/configuration'
 require 'solidus_globalize/engine'
 require 'solidus_globalize/version'
+require 'solidus_globalize/fallbacks'
--- lib/solidus_globalize/engine.rb
+++ lib/solidus_globalize/engine.rb
@@ -6,8 +6,7 @@ require 'friendly_id/globalize'
 module SolidusGlobalize
   class Engine < Rails::Engine
     engine_name 'solidus_globalize'
-
-    config.autoload_paths += %W(#{config.root}/lib)
+    include SolidusSupport::EngineExtensions::Decorators
 
     initializer "solidus_globalize.environment", before: :load_config_initializers do |_app|
       SolidusGlobalize::Config = SolidusGlobalize::Configuration.new
@@ -51,13 +50,5 @@ module SolidusGlobalize
       }
       Spree::PermittedAttributes.store_attributes << store_attributes
     end
-
-    def self.activate
-      Dir.glob(File.join(root, "app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare(&method(:activate).to_proc)
   end
 end
