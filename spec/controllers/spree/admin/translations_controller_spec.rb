87ee8fc8e88616f9aa78476959704a9148e2130d
Alberto Vena <kennyadsl@gmail.com>
2020-01-16 16:52:28 +0100

    Allow set custom permission to allow/deny translations access
    The backend/app/controllers/spree/admin/base_controller.rb uses the
    model_class to authorize an admin for a given action.
    
    To allow a user with a custom PermissionSet to access the translations
    an `alias_method :model_class, :klass` is enough.
    
    In this way if a PermissionSet say:
    
    class CustomPermission < Spree::PermissionSets::Base
      def activate!
        can :manage, Spree::Product
      end
    end
    
    the user will be able to :manage the Spree::Product::Translation too.
    
    Co-authored-by: oeN <oeN@users.noreply.github.com>

---
+++ spec/controllers/spree/admin/translations_controller_spec.rb
@@ -0,0 +1,38 @@
+module Spree
+  RSpec.describe Admin::TranslationsController, type: :controller do
+    let!(:product) { create(:product) }
+
+    context 'as a full admin' do
+      stub_authorization!
+
+      it 'displays page successfully' do
+        get :index, params: { resource: 'products', resource_id: product.slug }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'as a limited admin' do
+      context 'when I cannot manage products' do
+        stub_authorization! do |_|
+          cannot :manage, Spree::Product
+        end
+
+        it 'redirect to authorization failure' do
+          get :index, params: { resource: 'products', resource_id: product.slug }
+          expect(response).to redirect_to '/unauthorized'
+        end
+      end
+
+      context 'when I can manage products' do
+        stub_authorization! do |_|
+          can :manage, Spree::Product
+        end
+
+        it 'displays page successfully' do
+          get :index, params: { resource: 'products', resource_id: product.slug }
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+  end
+end
