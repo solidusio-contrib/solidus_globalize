d38dc3d1e02248984b7cc351e465637ed4e115f6
Zach Callanan <caveofmonsters@gmail.com>
2021-01-18 14:28:26 +0100

    Added support for shipping method and associated translations discard

--- app/decorators/models/solidus_globalize/spree/shipping_method_decorator.rb
+++ app/decorators/models/solidus_globalize/spree/shipping_method_decorator.rb
@@ -7,6 +7,28 @@ module SolidusGlobalize
         base.class_eval do
           translates :name, fallbacks_for_empty_translations: true
           include SolidusGlobalize::Translatable
+
+          after_discard do
+            # Discard associated records
+            translations.discard_all
+          end
+
+          translation_class.class_eval do
+            # Paranoia soft delete
+            acts_as_paranoid
+
+            # Warn users of future Paranoia gem removal
+            include ::Spree::ParanoiaDeprecations
+
+            # Account for Paranoia migration
+            include Discard::Model
+            self.discard_column = :deleted_at
+
+            # Paranoid sets the default scope and Globalize rewrites all query methods.
+            # Therefore we prefer to unset the default_scopes over injecting 'unscope'
+            # in every Globalize query method.
+            self.default_scopes = []
+          end
         end
       end
 
