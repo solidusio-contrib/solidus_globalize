845c4b34d49a0e4d1cd5c01888070f070918b524
Graham Bouvier <graham@super.gd>
2021-09-16 16:27:30 -0700

    Utilize configuration object for translatable fields
    Previously, translatable fields were hard-coded in the various decorators.
    With this change, they can be overridden with configuration in the client application
    using similar configuration patterns to other solidus extensions.

--- app/decorators/models/solidus_globalize/spree/option_type_decorator.rb
+++ app/decorators/models/solidus_globalize/spree/option_type_decorator.rb
@@ -4,8 +4,9 @@ module SolidusGlobalize
   module Spree
     module OptionTypeDecorator
       def self.prepended(base)
+        translatable_fields = ::SolidusGlobalize::Config.translatable_fields[base.to_s]
         base.class_eval do
-          translates :name, :presentation, fallbacks_for_empty_translations: true
+          translates(*translatable_fields, fallbacks_for_empty_translations: true)
           include SolidusGlobalize::Translatable
         end
       end
--- lib/solidus_globalize/configuration.rb
+++ lib/solidus_globalize/configuration.rb
@@ -10,5 +10,38 @@ module SolidusGlobalize
     # e.g. If available_locales are [:en, :es] admin can translate model records
     # to spanish as well. Once it's done :es can be added to supported_locales
     preference :supported_locales, :array, default: [:en]
+
+    preference(:translatable_fields, :hash,
+      default: {
+        'Spree::OptionType' => [:name, :presentation],
+        'Spree::OptionValue' => [:name, :presentation],
+        'Spree::Product' => [
+          :name,
+          :description,
+          :meta_description,
+          :meta_keywords,
+          :meta_title,
+          :slug
+        ],
+        'Spree::ProductProperty' => [:value],
+        'Spree::Promotion' => [:name, :description],
+        'Spree::Property' => [:name, :presentation],
+        'Spree::ShippingMethod' => [:name],
+        'Spree::Store' => [
+          :name,
+          :meta_description,
+          :meta_keywords,
+          :seo_title
+        ],
+        'Spree::Taxon' => [
+          :name,
+          :description,
+          :meta_title,
+          :meta_description,
+          :meta_keywords,
+          :permalink
+        ],
+        'Spree::Taxonomy' => [:name],
+      })
   end
 end
